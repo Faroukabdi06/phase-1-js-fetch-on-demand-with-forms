1bed87898874c06e3e6f9761af3ac844d72116f6
Farouk Abdi <abdifarouk58@gmail.com>
2025-04-03 14:26:20 +0300

    index.js updated

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 const init = () => {
-  
+  const inputForm = document.querySelector('form')
+//   console.log(inputForm)
+// let idInput  = inputForm[0];
+let idInput = document.querySelector("input#searchByID");
+console.log(idInput)
+  inputForm.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    // console.log(e);
+//    console.log(idInput.value)
+
+    fetch(`http://localhost:3000/movies/${idInput.value}`)
+    .then((response)=>response.json())
+    .then((data)=>{
+        // console.log(data)
+        const title =document.querySelector('#movieDetails h4');
+        const summary = document.querySelector('#movieDetails p');
+
+        title.textContent = data.title;
+        summary.textContent= data.summary;
+    })
+
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
